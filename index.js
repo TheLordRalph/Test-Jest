598f6e6b913aaecd48f8a8d8969a9fc35d4cc233
TheLordRalph <raulmoretamartin@gmail.com>
2023-01-09 18:49:32 +0100

    Metodos isOccupied y occupancyPercentage terminados y con sus test funcionando.

--- index.js
+++ index.js
@@ -7,11 +7,24 @@ export class Room {
     }
 
     isOccupied(date) {
-
+        for (let i = 0; i < this.bookings.length; i++) {
+            if (date >= this.bookings[i].checkIn && date <= this.bookings[i].checkOut) {
+                return true;
+            } 
+        }
+        return false;
     }
 
     occupancyPercentage(startDate, endDate) {
+        const numeroDeDiasSeleccionados = (endDate.getTime() - startDate.getTime()) / (1000 * 3600 * 24);
+        let date = new Date(startDate);
+        let countDays = 0;
+        for (let i = 0; i < numeroDeDiasSeleccionados; i++) {
+            if (this.isOccupied(date)) { countDays += 1 };
+            date.setDate(date.getDate() + 1);
+        }
 
+        return (countDays * 100) / (numeroDeDiasSeleccionados + 1);
     }
 
     static totalOccupancyPercentage(rooms, startDate, endDate) {
--- index.test.js
+++ index.test.js
@@ -1,18 +1,19 @@
 import { Room, Booking } from './index';
 
 const newRoom = new Room('E32', [], 123.23, 20);
-const newBooking = new Booking("Felipe", "felipe@gmail.com", '2022-02-03', '2022-02-06', 10, newRoom);
+const newBooking = new Booking("Felipe", "felipe@gmail.com", new Date('2022-02-03'), new Date('2022-02-06'), 10, newRoom);
+newRoom.bookings.push(newBooking);
 
 test('Room is occupied', function() {
-    expect(newRoom.isOccupied('2022-02-04')).toBeTruthy();
+    expect(newRoom.isOccupied(new Date('2022-02-04'))).toBeTruthy();
 });
 
 test('Room is not occupied', function() {
-    expect(newRoom.isOccupied('2022-02-04')).not.toBeTruthy();
+    expect(newRoom.isOccupied(new Date('2022-02-02'))).not.toBeTruthy();
 });
 
 test('Room percentage of occupancy', function() {
-    expect(newRoom.occupancyPercentage('2022-02-03', '2022-02-06')).toBe(100);
+    expect(newRoom.occupancyPercentage(new Date('2022-02-03'), new Date('2022-02-12'))).toBe(40);
 });
 
 test('Total room percentage of occupancy', function() {
